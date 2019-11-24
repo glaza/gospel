980cb17810e24b5e820bbfa660388ab80dfd4a5e
George Laza <george.laza@intelerad.com>
2019-11-23 21:13:45 -0500

    Refactored code

--- postprocessing/postprocess.py
+++ postprocessing/postprocess.py
@@ -3,9 +3,11 @@
 import sys
 import re
 import os
-TIME_LAPSE_CAMERA = True
-RETRACTION = 5 #mm
 
+TIME_LAPSE_CAMERA = True
+TIME_REMAINING = True
+RETRACTION = 5 #mm
+FILE_NAME_PREFIX = "p"
 
 def fetch(regex, line):
     label_match = re.search("\\(\\?P\\<(?P<label>.+)\\>", regex)
@@ -35,61 +37,55 @@ def output_line(output_file, line):
     output_file.write(line + "\n")
 
 def process(input_file, output_file):
-    time = None
+    time_total = None
     time_left_seconds = 0
     time_left_minutes = 0
-    layer_count = None
     last_g_line = None
     last_extruder_position = None
 
     for line in input_file:
         line = line.strip()
 
-        if not time:
-            time = fetch(";TIME:(?P<time_elapsed>\\d*)", line)
+        if TIME_REMAINING:
+            if not time_total:
+                time_total = fetch(";TIME:(?P<time_total>\\d*)", line)
 
-        if not layer_count:
-            layer_count = fetch(";LAYER_COUNT:(?P<layer_count>\\d+)", line)
+            time_elapsed = fetch(";TIME_ELAPSED:(?P<time_elapsed>\\d*)", line)
+            if time_elapsed:
+                time_left = float(time_total) - float(time_elapsed)
+                time_left_seconds = time_left % 60
+                time_left_minutes = time_left // 60
 
-        # Example G0 and G1 lines
-        # -----------------------
-        # G1 F1200 X97.996 Y131.371 E149.00487
-        # G0 F6000 X97.561 Y131.428
-        # G0 X95.882 Y124.631
-        if re.match("G[01]( [FXYZE]\\d+(\\.\\d+)*)+", line):
-            last_g_line = line
-            e_matcher = re.match(".*E(?P<extruder_pos>\\d+\\.\\d+).*", line)
-            if e_matcher:
-                last_extruder_position = float(e_matcher.group("extruder_pos"))
+                output_line(output_file, line)
+                message(output_file, 
+                        "{percent:2.1%} " +
+                        "{time_left_minutes:5.0f}m " +
+                        "{time_left_seconds:2.0f}s",
+                        {
+                            'percent': float(time_elapsed) / float(time_total),
+                            'time_left_seconds': time_left_seconds,
+                            'time_left_minutes': time_left_minutes
+                        })
+                continue
 
-        if re.match("G1 X0 Y220 ;Present print", line):
-            output_line(output_file, line)
-            output_line(output_file, "G1 X10 Y220 ;GEO: Stop pressing the sutter")
-            continue
+        if TIME_LAPSE_CAMERA: 
+            # Match a move or extrude line to save the last positions 
+            #
+            # Example G0 and G1 lines
+            # -----------------------
+            # G1 F1200 X97.996 Y131.371 E149.00487
+            # G0 F6000 X97.561 Y131.428
+            # G0 X95.882 Y124.631
+            if re.match("G[01]( [FXYZE]\\d+(\\.\\d+)*)+", line):
+                last_g_line = line
+                e_matcher = re.match(".*E(?P<extruder_pos>\\d+\\.\\d+).*", line)
+                if e_matcher:
+                    last_extruder_position = float(e_matcher.group("extruder_pos"))
 
-        time_elapsed = fetch(";TIME_ELAPSED:(?P<time_elapsed>\\d*)", line)
-        if time_elapsed:
-            time_left = float(time) - float(time_elapsed)
-            time_left_seconds = time_left % 60
-            time_left_minutes = time_left // 60
-
-            output_line(output_file, line)
-            message(output_file, 
-                    "{percent:2.1%} " +
-                    "{time_left_minutes:5.0f}m " +
-                    "{time_left_seconds:2.0f}s",
-                    {
-                        'percent': float(time_elapsed) / float(time),
-                        'time_left_seconds': time_left_seconds,
-                        'time_left_minutes': time_left_minutes
-                    })
-            continue
-
-        layer_index = fetch(";LAYER:(?P<layer_index>\\d*)", line)
-        if layer_index:
-            output_file.write(line)
-            output_file.write("\n")
-            if TIME_LAPSE_CAMERA:
+            # Match the layer index to insert the button press
+            layer_index = fetch(";LAYER:(?P<layer_index>\\d*)", line)
+            if layer_index:
+                output_line(output_file, line)
                 if last_extruder_position:
                     e(output_file,
                       last_extruder_position - RETRACTION,
@@ -100,7 +96,15 @@ def process(input_file, output_file):
                 output_line(output_file, last_g_line)
                 if last_extruder_position:
                     e(output_file, last_extruder_position, "Unretract 5mm")
-
+                continue
+            
+            # Match the very last move line to ensure we stop pressing the button
+            if re.match("G1 X0 Y220 ;Present print", line):
+                output_line(output_file, line)
+                output_line(output_file, "G1 X10 Y220 ;GEO: Stop pressing the sutter")
+                continue
+        
+        # Nothing left to do, write out the line
         output_line(output_file, line)
 
 if __name__ == "__main__":
@@ -116,7 +120,7 @@ if __name__ == "__main__":
     input_file = open(file_path, "r")
 
     output_file = open(os.path.dirname(file_path) +
-                       "/processed_" +
+                       "/" + FILE_NAME_PREFIX +
                        os.path.basename(file_path), "w")
     process(input_file, output_file)
     input_file.close()
